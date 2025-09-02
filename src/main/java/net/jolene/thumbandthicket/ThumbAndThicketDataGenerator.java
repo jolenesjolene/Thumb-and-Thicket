82fdd2f0aeb80bd173376b636d48186888402b8c
Jose <lloydjoseph137@gmail.com>
2025-09-02 20:19:56 +0100

    Initial commit

---
+++ src/main/java/net/jolene/thumbandthicket/ThumbAndThicketDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jolene.thumbandthicket;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThumbAndThicketDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
