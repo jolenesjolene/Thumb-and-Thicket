aa909c83f562b1d1e3284e21f5a8457f6f047830
Lily <lilyschlossnagel@icloud.com>
2026-03-18 16:43:41 +0100

    mod compatible log cutoutage! (also fixed the roots folder stuff)

--- src/main/java/net/jolene/thumbandthicket/ThumbAndThicketClient.java
+++ src/main/java/net/jolene/thumbandthicket/ThumbAndThicketClient.java
@@ -2,15 +2,49 @@ package net.jolene.thumbandthicket;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.jolene.thumbandthicket.block.ModBlocks;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.Block;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+
+import static net.jolene.thumbandthicket.ThumbAndThicket.MOD_ID;
 
 public class ThumbAndThicketClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.BIRCH_LOG, RenderLayer.getCutout());
+
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> client.execute(() -> {
+            for (RegistryEntry<Block> entry : Registries.BLOCK.iterateEntries(BlockTags.LOGS)) {
+                Block block = entry.value();
+                BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
+            }
+        }));
+
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(
+                new SimpleSynchronousResourceReloadListener() {
+                    @Override
+                    public Identifier getFabricId() {
+                        return Identifier.of(MOD_ID, "log_reload_listener");
+                    }
+
+                    @Override
+                    public void reload(ResourceManager manager) {
+                        for (RegistryEntry<Block> entry : Registries.BLOCK.iterateEntries(BlockTags.LOGS)) {
+                            Block block = entry.value();
+                            BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout());
+                        }
+                    }
+                }
+        );
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROOT_BLOCK, RenderLayer.getCutout());
     }
 }
