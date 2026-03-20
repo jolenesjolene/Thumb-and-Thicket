eabf86ea8b22edde379a155261236f8ccce2021a
jolenesjolene <imfullof137@gmail.com>
2026-03-20 13:20:22 +0000

    acorns!

--- src/main/java/net/jolene/thumbandthicket/item/ModItemGroups.java
+++ src/main/java/net/jolene/thumbandthicket/item/ModItemGroups.java
@@ -19,6 +19,7 @@ public class ModItemGroups {
                         .entries((displayContext, entries) -> {
                             entries.add(ModBlocks.ROOT_BLOCK);
                             entries.add(ModBlocks.ROOTED_GRASS);
+                            entries.add(Items.OAK_SAPLING);
                             entries.add(Items.DANDELION);
                             entries.add(ModBlocks.PUFFED_DANDELION);
                             entries.add(Items.LILAC);
