142b9bcc5dc61b0a59229258508e084aeaac45e2
jol <imfullof137@gmail.com>
2026-08-16 00:28:19 +0100

    pumpkin

--- src/main/java/net/jolene/thumbandthicket/ThumbAndThicketClient.java
+++ src/main/java/net/jolene/thumbandthicket/ThumbAndThicketClient.java
@@ -116,7 +116,7 @@ public class ThumbAndThicketClient implements ClientModInitializer {
                     }
                 }
         );
-
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.PUMPKIN, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.DUCKWEED, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROOT_BLOCK, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROOTED_GRASS, RenderLayer.getCutout());
